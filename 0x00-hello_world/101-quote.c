8149dcc386ccc61593d19cca4820cda4bad6fed6
erickson-m <mutisyaerickson@gmail.com>
2022-09-09 13:17:18 +0000

    used fprintf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints to stderr
+ *
+ * Description: uses fprintf instead of printf or puts
+ * Return: returns 1
+ */
+int main(void)
+{
+	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return(1);
+}
