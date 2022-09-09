a27ef598d84b5220d4c12fa6a2e7e3736b0948a4
erickson-m <mutisyaerickson@gmail.com>
2022-09-09 12:04:04 +0000

    used printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - this is the main function
+ *
+ *Description: uses the printf function to display in stdout
+ *Return: returns 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,");
+	return (0);
+}
