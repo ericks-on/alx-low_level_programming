632027b7158259a538472d9da5d340e8c94e55b3
erickson-m <mutisyaerickson@gmail.com>
2022-09-09 12:49:01 +0000

    k

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /**
  * main - prints sizes of datatypes
  *
- * Description: uses the functions printf and sizeof() to print sizes of various *data types
+ * Description: uses the functions printf and sizeof()
  * Return: returns 0
  */
 int main(void)
@@ -12,10 +12,10 @@ int main(void)
 	int li = sizeof(long int);
 	int lli = sizeof(long long int);
 	int f = sizeof(float);
-	printf("Size of a char: %d byte(s)\n",c);
-	printf("Size of an int: %d byte(s)\n",i);
-	printf("Size of a long int: %d byte(s)\n",li);
-	printf("Size of a long long int: %d byte(s)\n",lli);
-	printf("Size of a float: %d byte(s)\n",f);
+	printf("Size of a char: %d byte(s)\n", c);
+	printf("Size of an int: %d byte(s)\n", i);
+	printf("Size of a long int: %d byte(s)\n", li);
+	printf("Size of a long long int: %d byte(s)\n", lli);
+	printf("Size of a float: %d byte(s)\n", f);
 	return (0);
 }
