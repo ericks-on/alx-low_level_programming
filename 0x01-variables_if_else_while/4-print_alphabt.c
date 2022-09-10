63b98700d99a998fa298269224e8cc5296eb65ed
erickson-m <mutisyaerickson@gmail.com>
2022-09-10 00:50:48 +0000

    except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - k
+ *
+ * Description: k
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' || c != 'e')
+			putchar(c);
+	}
+	return (0);
+}
