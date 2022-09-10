c99e5e05df976abc93d137443d9a40f4a4f6d3d6
erickson-m <mutisyaerickson@gmail.com>
2022-09-10 01:18:45 +0000

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - k
+ *
+ * Description: k
+ * Return: k
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n < '10'; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
