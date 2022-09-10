d35e65ade432feaf93adb75dfdb7bef7a3b19a7f
Erickson Mbuvi <99821444+ericks-on@users.noreply.github.com>
2022-09-11 02:34:54 +0300

    two digit combos

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combos of two digits
+ *
+ * Description: uses putchar and for loop
+ * Return: returns 0
+ */
+int main(void)
+{
+	int n;
+	
+	for (n = '0'; n < '9'; n++)
+	{
+		putchar(n);
+		for (n = n + 1; n <= '9'; n++)
+		{
+			putchar(n);
+			if (n == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
