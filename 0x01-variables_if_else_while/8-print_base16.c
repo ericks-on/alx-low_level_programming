4fe550431c42a8cbb8aa1ce2fb2ee8540cd92d40
erickson-m <mutisyaerickson@gmail.com>
2022-09-10 01:37:31 +0000

    base 16 numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - base 16 numbers
+ *
+ * Description: printint all base 16 numbers using putchar
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	int c;
+
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+	for (c = 'a'; c <= 'f'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0)
+}
