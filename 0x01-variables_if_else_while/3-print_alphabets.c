246c3bdd10fed3064f73e42eb635f1b6ea675d0d
erickson-m <mutisyaerickson@gmail.com>
2022-09-10 00:39:20 +0000

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - listing all letters
+ *
+ * Description: using putchar only to list alphabets
+ * Return: returned 0
+ */
+int main(void)
+{
+	int c;
+	
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+	putchar('\n');
+
+}
