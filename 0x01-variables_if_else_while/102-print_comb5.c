75717000ee4b94787abf2ac9b5c39df37f901da3
erickson-m <mutisyaerickson@gmail.com>
2022-09-12 00:41:27 +0000

    ensured betty style

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,11 +15,12 @@ int main(void)
 
 	for (first = 0; first <= 9; first++)
 	{
-		for (second = 0; second <= 9; second++)
+		for (second = first + 1; second <= 9; second++)
 		{
-			for (third = 0; third <= 9; third++)
+			for (third = second + 1; third <= 9; third++)
 			{
-				for (forth = 0; forth <= 9; forth++)
+				for (forth = third + 1; forth <= 9;
+						forth++)
 				{
 					putchar((first % 10) + '0');
 					putchar((second % 10) + '0');
