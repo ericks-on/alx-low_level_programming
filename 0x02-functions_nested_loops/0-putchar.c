e1bf0d92a80f20747e893805e1c782d287d624ce
erickson-m <mutisyaerickson@gmail.com>
2022-09-12 17:58:59 +0000

    printing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - printing on the stdout
+ *
+ * Description: used printf function
+ * Return: returns 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
