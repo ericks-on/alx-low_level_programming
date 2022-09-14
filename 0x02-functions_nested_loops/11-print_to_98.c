6c411f412f833460e034f4c8285f92dcf63a0838
erickson-m <mutisyaerickson@gmail.com>
2022-09-14 21:30:55 +0000

    included numbers larger than 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,12 +8,26 @@
  */
 void print_to_98(int n)
 {
-	for (n = n; n <= 98; n++)
+	if (n <= 98)
 	{
-		if (n < 98)
-			printf("%d, ", n);
-		else
-			printf("%d", n);
+		for (n = n; n <= 98; n++)
+		{
+			if (n < 98)
+				printf("%d, ");
+			else
+				printf("%d");
+		}
+		printf("\n");
 	}
-	printf("\n");
+	else
+	{
+		for (n = n; n >= 98; n--)
+		{
+			if (n > 98)
+				printf("%d, ");
+			else
+				printf("%d");
+		}
+		printf("\n");
+	}
 }
