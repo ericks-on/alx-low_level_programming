4c69a588405298b3628a105398f4f7ff2c8a7b98
Erickson Mbuvi <mutisyaerickson@gmail.com>
2022-09-17 14:16:31 +0300

    multipying

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: requires first factor
+ * @b: requires second factor
+ *
+ * Return: returns product
+ */
+int mul(int a, int b)
+{
+	int ans;
+
+	ans = a * b;
+	return (ans);
+}
