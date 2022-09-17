e0577e5fd8f46e616af27268d5f3bf74f4c33a93
Erickson Mbuvi <mutisyaerickson@gmail.com>
2022-09-17 15:01:59 +0300

    largest prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factors.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - program that finds and prints the largest prime factor of num
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	long i;
+	long num = 612852475143;
+
+	for (i = 2; i < num; i++)
+	{
+		if (num % i == 0)
+			num /= i;
+	}
+	printf("%li\n", num);
+	return (0);
+}
