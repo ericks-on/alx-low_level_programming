4de45829727ea7fbb7eabb43cd8d8e3ce4d467fa
erickson-m <mutisyaerickson@gmail.com>
2022-09-19 22:03:22 +0000

    getting size of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - returns length of a string
+ * @s: string to get its length
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	return (sizeof(s));
+}
