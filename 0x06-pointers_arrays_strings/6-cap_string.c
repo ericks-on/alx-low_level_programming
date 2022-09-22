fd3661e15089475c37e9aa0574538b3d7773a0c1
erickson-m <mutisyaerickson@gmail.com>
2022-09-22 22:47:16 +0000

    caps every letter

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * cap_string - capitalized all words of a string
+ * @s: string to be capitalized
+ *
+ * Return: the converted string
+ */
+char *cap_string(char *s)
+{
+	int l = 0;
+	int i;
+
+	while (s[l] != 0)
+		l++;
+	for (i = 0; i < l; i++)
+	{
+		if (s[0] >= 'a' && s[0] <= 'z')
+			s[0] = 'A' + (s[0] - 'a');
+		else if ((s[i] = ' ') || (s[i] = '\n') || (s[i] = '\t'))
+		{
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			{
+				s[i + 1] = 'A' + (s[i + 1] - 'a');
+			}
+		}
+	}
+	return (s);
+}
