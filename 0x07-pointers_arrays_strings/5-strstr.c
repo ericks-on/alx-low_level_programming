d52d7d5aae2c17e7c35dea6e4d3befaacad154c0
erickson-m <mutisyaerickson@gmail.com>
2022-09-27 12:34:46 +0000

    another fix on strstr file

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,20 +8,23 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i;
 	int j = 0;
+	int n;
 
 	while (needle[i] != '\0')
 		j++;
-	while (*haystack != '\0')
+	for (n = 0; haystack[n] != '\0'; n++)
 	{
 		for(i = 0; i < j; i++)
 		{
-			if (*(haystack + i) != needle[i])
+			if (haystack[n] != needle[i])
 				break;
+			else
+				haystack++;
 		}
-		if (!=needle[j])
-			return haystack;
+		if (i == j)
+			return(haystack - i);
 		else
-			haystack++;
+			haystack--;
 	}
 	return (NULL);
 }
