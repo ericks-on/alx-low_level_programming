ff3f71b2c9a47d256aea756c836a95791dfdf37f
erickson-m <mutisyaerickson@gmail.com>
2022-09-27 15:15:25 +0000

    diagonal sums of a matrix

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - prints sum of diagonals of a matrix
+ * @a: the matrix
+ * @size: size of the matrix
+ *
+ * Return: no return value
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j;
+	int n = size;
+	int sum_1 = 0;
+	int sum_2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+				sum_1 += a[i][j];
+		}
+		n--;
+		sum_2 += a[i][n];
+	}
+	printf("%d, %d", sum_1, sum_2);
+}
