951afa369cb1c33b53adb4cbc9c36262c36c521b
erickson-m <mutisyaerickson@gmail.com>
2022-10-04 22:33:59 +0000

    betty style

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	a = malloc(sizeof(char) * size);
 	if (a == NULL)
-		return(NULL);
+		return (NULL);
 	for (i = 0; i < size; i++)
 		a[i] = c;
 	return (a);
