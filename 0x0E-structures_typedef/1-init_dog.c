8ca4808836e3fb88c16c8429c04018b429682972
erickson-m <mutisyaerickson@gmail.com>
2022-10-12 20:50:25 +0000

    another struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to the variable of type struct dog
+ * @name: the name of d
+ * @age: age of d
+ * @owner: owner of d
+ *
+ * Return: nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = malloc(typeof(struct dog));
+	if (d == NULL)
+		return (NULL);
+	d->name = *name;
+	d->age = age;
+	d->owner = *owner;
+}
