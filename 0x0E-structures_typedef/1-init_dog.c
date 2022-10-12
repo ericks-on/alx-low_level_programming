8053e634bd03953874c98c1afffa30131cbeeac3
erickson-m <mutisyaerickson@gmail.com>
2022-10-12 20:58:02 +0000

    another struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,9 +11,6 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(typeof(struct dog));
-	if (d == NULL)
-		return (NULL);
 	d->name = *name;
 	d->age = age;
 	d->owner = *owner;
