2c5df5ac0b0e069fffcf8b1251d910deea6d6100
erickson-m <mutisyaerickson@gmail.com>
2022-10-25 23:07:41 +0000

    unsuccessful on last task

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to list_t list
+ *
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+
+}
