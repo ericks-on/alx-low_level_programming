e661f16548a1e9bda9809ddefd344d351082fe63
erickson-m <mutisyaerickson@gmail.com>
2022-10-25 19:05:39 +0000

    debugged the code

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,18 +3,16 @@
 #include <stdio.h>
 /**
  * get_len - gets length of a string stored in node of list_t
- * ptr: pointer node in linked list
+ * @s: string stored in node in linked list
  *
  * Return: the length of string
  */
-size_t get_len(list_t *ptr)
+size_t get_len(char *s)
 {
 	size_t len = 0;
-	char *s;
 
-	if ((ptr -> str) != NULL)
+	if (s)
 	{
-		s = ptr -> str;
 		while (s[len] != '\0')
 			len++;
 	}
@@ -32,19 +30,21 @@ size_t print_list(const list_t *h)
 	list_t *temp_ptr;
 	unsigned int l;
 	char *s;
+	char *head_str;
 
 	temp_ptr = h -> next;
 	if ((h -> str) == NULL)
 		printf("[0] (nil)\n");
 	else
 	{
-		printf("[%u] %s\n", (h -> len), (h -> str));
+		head_str = h -> str;
+		printf("[%lu] %s\n", get_len(head_str), (h -> str));
 	}
 	while (temp_ptr != NULL)
 	{
 		size++;
 		s = temp_ptr -> str;
-		l = get_len(temp_ptr);
+		l = get_len(s);
 		if ((s) == NULL)
 		{
 			printf("[0] (nil)");
