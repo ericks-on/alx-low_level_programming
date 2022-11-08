636931c72950f89a1662e6ca7b565b014530f6f5
erickson-m <mutisyaerickson@gmail.com>
2022-11-08 13:55:09 +0000

    file descriptors

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stddef.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+/**
+ * read_textfile - reads text file and prints it to std output
+ * @filename: name of file
+ * @letters: number of letters it should read and print
+ *
+ * Return:actual no of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t rb, wb;
+	char buff[1000]; 
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDWR);
+	if (fd == -1)
+		return (0);
+	rb = read(fd, buff, letters);
+	if (rb == -1)
+		return (0);
+	wb = write(STDOUT_FILENO, buff, rb);
+	if ((wb != rb) || wb == -1)
+		return (0);
+	return (wb);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,4 @@
+#include <stddef.h>
+#include <sys/types.h>
+int _putchar(char);
+ssize_t read_textfile(const char *filename, size_t letters);
